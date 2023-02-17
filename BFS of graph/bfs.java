073190f3bbe1c6a355d61e8493864fff5686d0f1
Lokesh <lokesh.immanuel555@gmail.com>
2023-02-17 18:28:44 +0530

    gfg

---
+++ BFS of graph/bfs.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        //List to store the answer
+        ArrayList<Integer> ans = new ArrayList<>();
+        // Queue to traverse breadth wise
+        Queue<Integer> q = new LinkedList<>();
+        //visited array to make sure not visiting a vertex more than once.
+        boolean visited[] = new boolean[V];
+        q.add(0);
+        visited[0] = true;
+        //BFS
+        while (q.size() > 0) {
+            int curr = q.remove();
+            ans.add(curr);
+            visited[curr] = true;
+            for (int nbr : adj.get(curr)) {
+                if(!visited[nbr]) {
+                    q.add(nbr);
+                    //mark as visited
+                    visited[nbr] = true;
+                }
+            }
+        }
+        return ans;
+    }
+}
