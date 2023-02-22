6d3c1d1789ac36d2f95890ba5f328850ad6313e3
Lokesh <lokesh.immanuel555@gmail.com>
2023-02-22 15:25:38 +0530

    feb 22

--- Graphs/Detect cycle in an undirected graph/Bit Manipulation/solution.java
+++ Graphs/Detect cycle in an undirected graph/Bit Manipulation/solution.java
@@ -1,12 +1,19 @@
 class Solution {
     static int setBits(int n) {
          int count = 0;
-		while (n != 0) {
-			if((n & 1) == 1) {
-				count++;
-			}
-			n>>=1;
-		}
+         int count = 0;
+         while (n > 0) {
+             count++;
+             //deletes rightmost set bit
+             n = n & (n - 1);
+         }
+         return count;
+		// while (n != 0) {
+		// 	if((n & 1) == 1) {
+		// 		count++;
+		// 	}
+		// 	n>>=1;
+		// }
 		return count;
     }
 }
